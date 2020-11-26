b7e2f71fe169580e85227d410f5e3785ed926b77
Niklas Pimenoff <pimenoff.niklas@gmail.com>
2020-11-26 17:26:19 +0200

    Putting all the new pieces together in the main. Server now implements the bad_api.ProductService. Availability yet to be added

--- warehouse_api/cmd/warehouse_api/main.go
+++ warehouse_api/cmd/warehouse_api/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 
+	"github.com/nikunicke/reaktor/warehouse_api/bad_api"
+	"github.com/nikunicke/reaktor/warehouse_api/bad_api/options"
 	"github.com/nikunicke/reaktor/warehouse_api/http"
 )
 
 func main() {
 	fmt.Println("warehouse_api started")
+	client, err := bad_api.NewClient(
+		options.Client().ApplyURI("https://bad-api-assignment.reaktor.com/"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	server := http.NewServer()
 	server.Addr = ":80"
+	server.ProductService = bad_api.NewProductService(client)
 	server.Open()
 
 	c := make(chan os.Signal, 1)
