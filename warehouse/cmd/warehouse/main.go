adfb152c60c2668c0cf4f294b641bb73798e2680
nikunicke <pimenoff.niklas@gmail.com>
2020-11-30 01:48:38 +0200

    putting the pieces together in MAIN. Still needs refactoring into a few initialization functions.

--- warehouse/cmd/warehouse/main.go
+++ warehouse/cmd/warehouse/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/nikunicke/reaktor/warehouse"
 	"github.com/nikunicke/reaktor/warehouse/api"
@@ -14,74 +15,39 @@ import (
 
 func main() {
 	fmt.Println("warehouse_api started")
+
 	client, err := api.NewClient(
 		options.Client().ApplyURI("https://bad-api-assignment.reaktor.com/"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	exec := warehouse.JobExecutor{
-		ProductService:      api.NewProductService(client),
-		AvailabilityService: api.NewAvailabilityService(client),
+	warehouse := warehouse.New(warehouse.ProductCategories...)
+	warehouse.ProductService = api.NewProductService(client)
+	warehouse.AvailabilityService = api.NewAvailabilityService(client)
+	warehouse.UpdateInterval = time.NewTicker(time.Minute * 5)
+	for {
+		if err := warehouse.Update(); err != nil {
+			fmt.Printf("Error: %v\n", err)
+			fmt.Println("Retrying...")
+		} else {
+			break
+		}
 	}
-	job := warehouse.Job{Type: warehouse.JobTypeGetAvailability}
-	exec.ExecuteJob(&job)
-	// c1 := make(chan string)
-	// c2 := make(chan string)
-	// c3 := make(chan string)
-	// c4 := make(chan string)
-	// c5 := make(chan string)
-	// c6 := make(chan string)
+	fmt.Println("init loop done")
+	go func() {
+		for {
+			fmt.Println("auto update")
+			select {
+			case <-warehouse.UpdateInterval.C:
+				warehouse.Update()
+			}
+		}
+	}()
 
-	// go func() {
-	// 	client.Get(warehouse.Jackets)
-	// 	for i := 0; i < 4; i++ {
-	// 		fmt.Println("processing availability:", <-c6)
-	// 	}
-	// 	c1 <- "Got jackets"
-	// }()
-
-	// go func() {
-	// 	client.Get("availability/derp")
-	// 	c6 <- "derp"
-	// 	c2 <- "Got availability/derp"
-	// }()
-
-	// go func() {
-	// 	client.Get("availability/reps")
-	// 	c6 <- "reps"
-	// 	c3 <- "Got availability/reps"
-	// }()
-
-	// go func() {
-	// 	client.Get("availability/xoon")
-	// 	c6 <- "xoon"
-	// 	c4 <- "Got availability/xoon"
-	// }()
-
-	// go func() {
-	// 	client.Get("availability/nouke")
-	// 	c6 <- "nouke"
-	// 	c5 <- "Got availability/nouke"
-	// }()
-
-	// for i := 0; i < 5; i++ {
-	// 	select {
-	// 	case msg1 := <-c1:
-	// 		fmt.Println("recieved:", msg1)
-	// 	case msg2 := <-c2:
-	// 		fmt.Println("received:", msg2)
-	// 	case msg3 := <-c3:
-	// 		fmt.Println("received:", msg3)
-	// 	case msg4 := <-c4:
-	// 		fmt.Println("received:", msg4)
-	// 	case msg5 := <-c5:
-	// 		fmt.Println("received:", msg5)
-	// 	}
-	// }
-	fmt.Println("done")
 	server := http.NewServer()
 	server.Addr = ":80"
+	server.WarehouseService = warehouse
 	server.ProductService = api.NewProductService(client)
 	server.AvailabilityService = api.NewAvailabilityService(client)
 	server.Open()
