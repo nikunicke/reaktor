f288d5a398262e99cfa79a89ffb205beed711325
nikunicke <pimenoff.niklas@gmail.com>
2020-12-01 00:45:47 +0200

    Should not have any bugs anymore. Previous implementation did not guarantee that all categories got availabilities from all manufacturers. Now they do, and there should not be any race conditions.

--- warehouse/warehouse.go
+++ warehouse/warehouse.go
@@ -3,6 +3,7 @@ package warehouse
 import (
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -33,66 +34,77 @@ func New(categories ...string) *Warehouse {
 }
 
 func (w *Warehouse) Update() error {
-	p := make(chan Products)
-	a := make(chan AvailabilityResponseMap)
-	e := make(chan error)
-	inventory := w.Inventory
+	a := make(chan []*AvailabilityResponseMap)
+	quit := make(chan bool)
+	done := make(chan error)
+	Availabilities := make([]*AvailabilityResponseMap, len(Manufacturers))
+	Inventory := make(Inventory)
+	var lock = sync.RWMutex{}
 
 	fmt.Println("Updating warehouse data")
 	for _, category := range ProductCategories {
-		go func(ctg string) {
-			products, err := w.ProductService.GetProducts(ctg)
-			if err != nil {
-				e <- err
-			}
-			for range Manufacturers {
-				select {
-				case availability := <-a:
-					{
-						for i, _ := range products {
-							key := strings.ToUpper(products[i].ID)
-							if _, ok := availability[key]; ok {
-								products[i].Availability = availability[key]
-							}
+		go func(category string) {
+			var err error
+			var products Products
+			if products, err = w.ProductService.GetProducts(category); err == nil {
+				availabilities := <-a
+				for _, availability := range availabilities {
+					for i, _ := range products {
+						key := strings.ToUpper(products[i].ID)
+						if _, ok := (*availability)[key]; ok {
+							products[i].Availability = (*availability)[key]
 						}
 					}
 				}
+				lock.Lock()
+				defer lock.Unlock()
+				Inventory[category] = products
 			}
-			p <- products
+			select {
+			case done <- err:
+			case <-quit:
+			}
 		}(category)
 	}
 
+	var i = 0
 	for m := range Manufacturers {
-		go func(manufacturer string) {
-			availability, err := w.AvailabilityService.GetAvailability(manufacturer)
-			if err != nil {
-				for range ProductCategories {
-					e <- err
-				}
+		fmt.Println(i)
+		go func(i int, manufacturer string) {
+			var value AvailabilityResponseMap
+			var availability *Availability
+			var err error
+			if availability, err = w.AvailabilityService.GetAvailability(manufacturer); err == nil {
+				value, err = availability.Response.Map()
+				Availabilities[i] = &value
 			}
-			for range ProductCategories {
-				time.Sleep(time.Second * 2)
-				value, err := availability.Response.Map()
-				if err != nil {
-					e <- err
-				}
-				a <- value
+			select {
+			case done <- err:
+			case <-quit:
 			}
-		}(m)
+		}(i, m)
+		i++
 	}
-
-	for range ProductCategories {
-		select {
-		case err := <-e:
-			{
-				fmt.Println("Update failed")
-				return err
-			}
-		case productData := <-p:
-			inventory[productData[0].Type] = productData
+	for success := 0; success < 5; success++ {
+		err := <-done
+		if err != nil {
+			close(quit)
+			fmt.Println("Updated failed")
+			return err
 		}
 	}
-	w.Inventory = inventory
+	a <- Availabilities
+	a <- Availabilities
+	a <- Availabilities
+	for success := 0; success < 3; success++ {
+		err := <-done
+		if err != nil {
+			close(quit)
+			fmt.Println("Updated failed")
+			return err
+		}
+	}
+	w.Inventory = Inventory
 	w.UpdatedAt = time.Now()
 	fmt.Println("Update complete")
 	return nil
